b0a8636bd8d6a20dedf041e07a00c5322471791c
bymixloltik6 <86566592+bymixloltik6@users.noreply.github.com>
2024-10-16 15:28:13 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('authButton').addEventListener('click', function() {
-    const authToken = 'Bearer HrU2uvjiJQFAz4vgog8HEhRj6LRdryUa4r4ZzwGFvzPu.3nT6yHQWwJJ6swL3WNKJ6yiFswar1RY4gbPh6mN4HXDWC2p.4PCC3odZSeRZLt9PhCZeDfB2acobWjGZHkNN6rvbqANnoZMKVmZagApZMczet3VGacdk9VV1KatJSY5Unfiy5qaK';
-    
-    alert(`Authorization: ${authToken}`);
-});
